494cc97a01f9a51c3d8192f0e1811934b4371b6e
unknown <sasham_c@yahoo.com>
2022-02-15 16:13:52 -0600

    added city pics

--- wayfarer-app/src/app/app.module.ts
+++ wayfarer-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { SlideshowDirective } from './slideshow.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideshowComponent,
+    SlideshowDirective
   ],
   imports: [
     BrowserModule,
---
+++ wayfarer-app/src/app/slideshow.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appSlideshow]'
+})
+export class SlideshowDirective {
+
+  constructor() { }
+
+}
