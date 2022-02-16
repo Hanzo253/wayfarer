c0f2feddb6c475abbe118456159c68692efcf866
David Doan <daviddoan45@gmail.com>
2022-02-16 12:04:52 -0500

    testing slideshow feature

--- wayfarer-app/src/app/header/header.component.ts
+++ wayfarer-app/src/app/header/header.component.ts
@@ -9,6 +9,8 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/1200/800`);
+
   ngOnInit(): void {
   }
 
