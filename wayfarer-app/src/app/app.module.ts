215802da12346048fc696bd2809ee83214adb1b0
Johnny <aguguaj@hotmail.com>
2022-02-16 10:38:32 -0600

    Saving work

--- wayfarer-app/src/app/app.module.ts
+++ wayfarer-app/src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CitiesComponent } from './cities/cities.component';
 import { HeaderComponent } from './header/header.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CitiesComponent
   ],
   imports: [
     BrowserModule,
