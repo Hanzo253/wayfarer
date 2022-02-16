dfbe5554d1d86f87decc56e608fed58caafe2e8e
Johnny <aguguaj@hotmail.com>
2022-02-16 16:32:07 -0600

    Working separate cities

--- wayfarer-app/src/app/app-routing.module.ts
+++ wayfarer-app/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CitiesComponent } from './cities/cities.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
 const routes: Routes = [
   {
     path: 'cities',
     component: CitiesComponent
+},
+{
+  path: '',
+  component: SlideshowComponent
 }
 ];
 
