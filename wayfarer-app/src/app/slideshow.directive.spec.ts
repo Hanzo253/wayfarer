71888b9548888bccd0913f71c4151f46fcbebb69
David Doan <daviddoan45@gmail.com>
2022-02-16 13:29:20 -0500

    handling more conflicts

--- wayfarer-app/src/app/slideshow.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SlideshowDirective } from './slideshow.directive';
-
-describe('SlideshowDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SlideshowDirective();
-    expect(directive).toBeTruthy();
-  });
-});
