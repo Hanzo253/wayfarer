01137d8a596107f43d1d4e2c22b9888e7046d53a
Johnny <aguguaj@hotmail.com>
2022-02-16 15:40:37 -0600

    stable

--- wayfarer-app/src/app/app.module.ts
+++ wayfarer-app/src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 import { CitiesComponent } from './cities/cities.component';
 import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DescriptionComponent } from './description/description.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-
 @NgModule({
   declarations: [
     AppComponent,
+    SlideshowComponent,
     HeaderComponent,
     CitiesComponent,
     DescriptionComponent,
