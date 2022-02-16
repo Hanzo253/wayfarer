e06d13fdf9baede211c6389daaf9e4710cd0f737
David Doan <daviddoan45@gmail.com>
2022-02-16 12:48:46 -0500

    merging

--- wayfarer-app/src/app/app.module.ts
+++ wayfarer-app/src/app/app.module.ts
@@ -6,15 +6,16 @@ import { AppComponent } from './app.component';
 import { CitiesComponent } from './cities/cities.component';
 import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-
-
+import { DescriptionComponent } from './description/description.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CitiesComponent
+    CitiesComponent,
+    DescriptionComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
